2e328bce73310c556f5fcce3a27270ca850de4ed
WaiEnu <strayhituji@gmail.com>
2025-04-05 09:23:12 +0900

    commit

--- src/components/todokunApp/todokun.tsx
+++ src/components/todokunApp/todokun.tsx
@@ -1,10 +1,16 @@
 import { useTask } from "../hooks/useTask";
+import { useEffect, useState } from "react";
 
 const TodoKun: React.FC = () => {
   const { todokun, generateId } = useTask();
+  const [message, setMasseage] = useState(todokun);
+  useEffect(() => {
+    setMasseage(todokun);
+    console.log(todokun,"app");
+  }, [todokun]);
   return (
     <div>
-      <div>{todokun}</div>
+      <div>{message}</div>
       <button onClick={generateId}>トドくん</button>
     </div>
   );
