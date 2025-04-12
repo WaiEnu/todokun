63e7029104d265d0db8105c984f3b85cdc1a2653
WaiEnu <53822854+WaiEnu@users.noreply.github.com>
2025-04-12 18:26:27 +0900

    page.tsx を更新

--- src/components/allApp/page.tsx
+++ src/components/allApp/page.tsx
@@ -1,13 +1,12 @@
-import IdProvider from "../context/useTodokun";
 import ToDo from '../todoApp/todoList';
 import Todokun from '../todokunApp/todokun';
 
 const Page: React.FC = () => {
   return (
-    <IdProvider>
+    <div>
       <Todokun />
       <ToDo />
-    </IdProvider>
+    </div>
   );
 };
 
