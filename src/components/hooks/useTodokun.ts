26d96568efd4076f3786c2400d0831d167e30d65
WaiEnu <53822854+WaiEnu@users.noreply.github.com>
2025-04-12 17:12:23 +0900

    useTodokun.ts を更新

--- src/components/hooks/useTodokun.ts
+++ src/components/hooks/useTodokun.ts
@@ -1,14 +1,7 @@
+import jotai from "../types/jotai"
+
 export const useTodokun = () => {
-const jotai = [
-      { id:0, text: "トドくんは人生という道に迷った", image: "/public/image/todokun0.PNG"},
-      { id:1, text: "トドくんはだらだらしている" , image: "/public/image/todokun1.PNG"},
-      { id:2, text: "トドくんは曲芸をしている", image: "/public/image/todokun2.PNG"},
-      { id:3, text: "トドくんは泳いでいる", image: "/public/image/todokun3.PNG"},
-      { id:4, text: "トドくんは寝ている", image: "/public/image/todokun4.PNG"},
-      { id:5, text: "トドくんは筋トレをしている", image: "/public/image/todokun5.PNG"},
-      { id:6, text: "トドくんは勉強をしている", image: "/public/image/todokun6.PNG"},
-      { id:7, text: "トドくんはマインドフルネスに目覚めている", image: "/public/image/todokun7.PNG" },
-  ]
+
   const generateRondomId = (length) => {
       const id = Math.random() * (length - 1);
       return Math.floor(id);
