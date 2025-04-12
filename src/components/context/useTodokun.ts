d44d036bf1f81fe96299118a5758466f96caf733
WaiEnu <53822854+WaiEnu@users.noreply.github.com>
2025-04-12 17:03:23 +0900

    useTodokun.ts を更新

--- src/components/context/useTodokun.ts
+++ src/components/context/useTodokun.ts
@@ -1,53 +1,35 @@
-//useContext
-interface TodokunContextType {
-  todokun: Todokun[];
-  setTodokun: (todokun: Todokun) => void;
-  generateTodokun: () => Todokun;
+import { createContext, ReactNode, useState } from "react";
+
+interface IdContextType {
+  id: number;
+  generateId: () => number;
 }
 
-const TodokunContext = createContext<TodokunContextType | undefined>(undefined);
+const IdContext = createContext<IdContextType>();
 
-export const TodokunProvider: React.FC<{ children: React.ReactNode }> = ({
+export const IdProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
- const jotai = [
-      { id:0, text: "トドくんは人生という道に迷った", image: "/public/image/todokun0.PNG"},
-      { id:1, text: "トドくんはだらだらしている" , image: "/public/image/todokun1.PNG"},
-      { id:2, text: "トドくんは曲芸をしている", image: "/public/image/todokun2.PNG"},
-      { id:3, text: "トドくんは泳いでいる", image: "/public/image/todokun3.PNG"},
-      { id:4, text: "トドくんは寝ている", image: "/public/image/todokun4.PNG"},
-      { id:5, text: "トドくんは筋トレをしている", image: "/public/image/todokun5.PNG"},
-      { id:6, text: "トドくんは勉強をしている", image: "/public/image/todokun6.PNG"},
-      { id:7, text: "トドくんはマインドフルネスに目覚めている", image: "/public/image/todokun7.PNG" },
-  ]
-  const generateRondomId = (length) => {
-      const id = Math.random() * (length - 1);
-      return Math.floor(id);
-  }
-  const generateRondom = () => {
-      const id = generateRondomId(jotai.length);
-      const j = jotai[id];
-      return j;
-  }
-  const [todokun, setTodokun] = useState(generateRondom());
-  const generateId = () => {
-      setTodokun(generateRondom());
+
+  const [id, setId] = useState(0);
+  const generateId = (length) => {
+      const id = Math.floor(Math.random() * (length - 1));
+      setId(id)
+      return id;
   };
 
-
   const value = {
-    todokun,
-    setTodokun,
+    id,
     generateId
   };
 
-  return <TodokunContext.Provider value={value}>{children}</TodokunContext.Provider>;
+  return <IdContext.Provider value={value}>{children}</IdContext.Provider>;
 };
 
 export const useTodokun = () => {
-  const context = useContext(TodokunContext);
+  const context = useContext(IdContext);
   if (context === undefined) {
-    throw new Error("useTodokun must be used within a TodokunProvider");
+    throw new Error("useTodokun must be used within a IdProvider");
   }
   return context;
 };
