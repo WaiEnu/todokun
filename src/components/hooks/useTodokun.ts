fdaa650f336ea9e8214f0786f4f2b102448c739f
WaiEnu <53822854+WaiEnu@users.noreply.github.com>
2025-04-12 18:25:48 +0900

    useTodokun.ts を更新

--- src/components/hooks/useTodokun.ts
+++ src/components/hooks/useTodokun.ts
@@ -1,19 +1,2 @@
-import jotai from "../types/jotai"
-
-export const useTodokun = () => {
-  const generateRondomId = (length) => {
-      const id = Math.random() * (length - 1);
-      return Math.floor(id);
-  }
-  const generateRondom = () => {
-      const id = generateRondomId(jotai.length);
-      const j = jotai[id];
-      return j;
-  }
-  const [todokun, setTodokun] = useState(generateRondom());
-  const generateId = () => {
-      setTodokun(generateRondom());
-  };
-
-  return { todokun, setTodokun, generateId  };
-};
+// npm install nanostores
+// npm i nanostores @nanostores/react
