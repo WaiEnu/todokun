5588e84a604d2a7962d885f80f67a6fbd06e2eb3
WaiEnu <53822854+WaiEnu@users.noreply.github.com>
2025-04-12 18:00:51 +0900

    todokun.tsx を更新

--- src/components/todokunApp/todokun.tsx
+++ src/components/todokunApp/todokun.tsx
@@ -3,15 +3,13 @@ import { useState } from "react";
 
 const TodoKun: React.FC = () => {
   const { todokun, generateId } = useTask();
-  const [message, setMasseage] = useState(todokun.text);
-  const [image, setImage] = useState(todokun.image);
 
   return (
     <div>
       <div>
-        <img src={image} width="150" height="150" alt="todokun" />
+        <img src={todokun.image} width="150" height="150" alt="todokun" />
       </div>
-      <div>{message}</div>
+      <div>{todokun.text}</div>
       <button onClick={generateId}>トドくん</button>
     </div>
   );
