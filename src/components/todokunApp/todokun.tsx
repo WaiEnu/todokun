ed0c36bc1e91e44c55eaa146de215e4d74c09472
WaiEnu <53822854+WaiEnu@users.noreply.github.com>
2025-04-12 18:30:24 +0900

    todokun.tsx を更新

--- src/components/todokunApp/todokun.tsx
+++ src/components/todokunApp/todokun.tsx
@@ -1,13 +1,14 @@
+import { useStore } from '@nanostores/react'
 import { todokun, generateTodokun } from "../context/useTodokun";
 
 const TodoKun: React.FC = () => {
-
+  const $todokun = useStore(todokun)
   return (
     <div>
       <div>
-        <img src={todokun.image} width="150" height="150" alt="todokun" />
+        <img src={$todokun.image} width="150" height="150" alt="todokun" />
       </div>
-      <div>{todokun.text}</div>
+      <div>{$todokun.text}</div>
       <button onClick={generateTodokun}>トドくん</button>
     </div>
   );
