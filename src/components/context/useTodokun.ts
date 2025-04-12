5aed0161bfa99a9354fd1bf31c48182e503cd549
WaiEnu <53822854+WaiEnu@users.noreply.github.com>
2025-04-12 16:40:31 +0900

    useTodokun.ts を更新

--- src/components/context/useTodokun.ts
+++ src/components/context/useTodokun.ts
@@ -5,5 +5,26 @@ interface TodokunContextType {
   generateTodokun: () => Todokun;
 }
 
-const TodoContext = createContext<TodokunContextType | undefined>(undefined);
+const TodokunContext = createContext<TodokunContextType | undefined>(undefined);
 
+export const TodokunProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [todokun, setTodokun] = useState<Todokun>(jotai[0]);
+
+
+  const value = {
+    todokun,
+    generateId
+  };
+
+  return <TodokunContext.Provider value={value}>{children}</TodokunContext.Provider>;
+};
+
+export const useTodokun = () => {
+  const context = useContext(TodokunContext);
+  if (context === undefined) {
+    throw new Error("useTodokun must be used within a TodokunProvider");
+  }
+  return context;
+};
